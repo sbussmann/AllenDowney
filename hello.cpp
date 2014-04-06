83bf53f0be75c4bb7501a4bf81e8f1293797512a
Shane Bussmann <sbussmann@gmail.com>
2014-04-05 20:56:24 -0400

    Chapter 9: More Structures
    Fun with hours, minutes, and seconds.  First discussion of algorithms.

--- hello.cpp
+++ hello.cpp
@@ -55,13 +55,62 @@ void swap (int& x, int&y)
     y = temp;
 }
 
+struct Time {
+    int hour, minute;
+    double second;
+};
+
+void printTime (Time& t) {
+    cout << t.hour << ":" << t.minute << ":" << t.second << endl;
+}
+
+bool after (Time& time1, Time& time2) {
+    if (time1.hour > time2.hour) return true;
+    if (time1.hour < time2.hour) return false;
+
+    if (time1.minute > time2.minute) return true;
+    if (time1.minute < time2.minute) return false;
+
+    if (time1.second > time2.second) return true;
+    return false;
+}
+
+Time addTime (const Time& t1, const Time& t2) {
+    double seconds = convertToSeconds (t1) + convertToSeconds (t2);
+    return makeTime (seconds);
+}
+
+double convertToSeconds (const Time& t) {
+    int minutes = t.hour * 60 + t.minute;
+    double seconds = minutes * 60 + t.second;
+    return seconds;
+}
+
+Time makeTime (double secs) {
+    Time time;
+    time.hour = int (secs / 3600.0);
+    secs -= time.hour * 3600.0;
+    time.minute = int (secs / 60.0);
+    secs -= time.minute * 60.0;
+    time.seconds = secs;
+    return time;
+}
+
+void increment (Time& time, double secs) {
+    time.second += secs % 60.0;
+}
+
 int main ()
 {
 
-    string name;
-    cout << "What's your name? ";
-    getline (cin, name);
-    cout << name << endl;
+    Time currentTime = { 9, 14, 30.0 };
+    Time breadTime = { 3, 35, 0.0 };
+    Time doneTime = addTime (currentTime, breadTime);
+    printTime (doneTime);
+    
+    increment (doneTime, 20.0);
+
+    printTime (doneTime);
 
     return 0;
 }
